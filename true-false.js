fb0edc7d9bb95a417a21e979f5735802b1c147fb
Amin Ruhul <67382736+aminruhul14@users.noreply.github.com>
2020-08-07 21:50:30 +0600

    true and false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+const age = 14;
+
+if (age) {
+    console.log("condition is true"); //any Number, string is true
+}
+else {
+    console.log("condition is false"); // 0 and empty string is false
+}
+
+const name = "Amin Ruhul"
+if (name) {
+    console.log("condition is true");
+
+}
+else {
+    console.log("condition is false");
+}
