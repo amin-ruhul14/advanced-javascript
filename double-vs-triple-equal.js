7f88262c33aa9bd2bd24c8adb9eed9bc3ef1f38c
Amin Ruhul <67382736+aminruhul14@users.noreply.github.com>
2020-08-08 00:22:38 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,12 @@
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+
+// double equal is only checked value 
+
+// triple equal is variable type 
